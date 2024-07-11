48de0f8794921b5b8450d5be0ed6fa8a4dbea04a
ct-seema-j <seema.jamakhandi@cardiotrack.io>
2024-07-11 13:19:30 +0400

    Changing port to 5000

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  //change port for production
+  preview: {
+    port: 5000,
+  },
+  // for dev
+  server: {
+    port: 5000,
+  },
 })
