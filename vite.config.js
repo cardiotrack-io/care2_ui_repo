b78ed60910a759d29089370a1cd9da844707bdf2
ct-seema-j <seema.jamakhandi@cardiotrack.io>
2024-07-11 14:11:38 +0400

    Fixing port

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  host: '0.0.0.0',
   //change port for production
   preview: {
     port: 4001,
