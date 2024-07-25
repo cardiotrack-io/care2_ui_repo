3cb0b0e70452265f4247f643182f3bde932a6916
ct-seema-j <seema.jamakhandi@cardiotrack.io>
2024-07-25 11:05:39 +0400

    Fixing backend url env

--- src/Constants/backEndURL.js
+++ src/Constants/backEndURL.js
@@ -5,5 +5,7 @@ console.log("RAZORPAY_KEY - " + import.meta.env.RAZORPAY_KEY)
 //const backEndUrl = 'https://api.dev.cardiotrack.link/clientuibackendsvc';//Dev
 // const backEndUrl = 'https://v2prod.cardiotrack.link/clientuibackendsvc';//Prod
 
-
+console.log(import.meta.env.MODE)
+console.log(import.meta.env.BASE_URL)
+console.log(import.meta.env.PROD)
 export default backEndUrl;
