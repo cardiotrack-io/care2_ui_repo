2597ca17b124106d2b6f1e27adfb28ba05f9aa42
ct-seema-j <seema.jamakhandi@cardiotrack.io>
2024-07-25 11:55:38 +0400

    Fixing backend url env

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,27 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: "/care",
-  plugins: [react()],
-  server: {
-    port: 4000,
-    host: true,
-  },
+// export default defineConfig({
+//   base: "/care",
+//   plugins: [react()],
+//   server: {
+//     port: 4000,
+//     host: true,
+//   },
 
-})
+// })
+
+export default ({ mode }) => {
+  // Load app-level env vars to node-level env vars.
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+  return defineConfig({
+    base: "/care",
+    plugins: [react()],
+    server: {
+      port: 4000,
+      host: true,
+    },
+
+  })
+}
