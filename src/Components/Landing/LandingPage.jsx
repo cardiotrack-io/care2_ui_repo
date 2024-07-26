84aed8199d388730ac448b70731052b15aeac6c2
Ganapati-cardiotrack <ganapati.hegde@cardiotrack.io>
2024-07-26 13:14:43 +0530

    Landing Page Fix

--- src/Components/Landing/LandingPage.jsx
+++ src/Components/Landing/LandingPage.jsx
@@ -9,7 +9,10 @@ function LandingPage({ setCurrentPage }) {
   return (
     <div className="relative flex flex-col items-center justify-center w-full h-screen overflow-hidden">
       <Header />
-      <div className="flip-in-hor-top absolute flex flex-col items-center -space-y-4 transform -translate-y-1/2 top-1/3">
+      <div 
+        className="flip-in-hor-top absolute flex flex-col items-center -space-y-4 transform -translate-y-1/2 top-1/3" 
+        style={{ top: 'calc(33.333% - 50px)' }}
+      >
         <div className="cardiotrack">
           <p className="font-bold text-black">Cardiotrack</p>
         </div>
@@ -24,7 +27,10 @@ function LandingPage({ setCurrentPage }) {
           </div>
         </div>
       </div>
-      <div className="absolute bottom-10 left-0 w-full pb-8 text-center">
+      <div 
+        className="absolute bottom-10 left-0 w-full pb-8 text-center" 
+        style={{ bottom: 'calc(2.5rem + 50px)' }}
+      >
         <div className="warning m-1">
           <p className="text-black">
             *by clicking on “Get Started” you have agreed to our T&C which are
