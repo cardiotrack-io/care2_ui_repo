3336fbbd0894e49090ff4c4374f25a65c7f98385
Ganapati-cardiotrack <ganapati.hegde@cardiotrack.io>
2024-07-27 17:45:04 +0530

    Landscape warning

--- src/Components/Utility/Header.jsx
+++ src/Components/Utility/Header.jsx
@@ -1,11 +1,12 @@
-import React, { useEffect, useState } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React, { useEffect, useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 // import { faWhatsapp } from '@fortawesome/free-brands-svg-icons';
-import { faHeadset } from '@fortawesome/free-solid-svg-icons';
-import './Header.css'; // Import the CSS file for styling
+import { faHeadset } from "@fortawesome/free-solid-svg-icons";
+import "./Header.css"; // Import the CSS file for styling
 
 const Header = () => {
   const [isVisible, setIsVisible] = useState(true);
+  const [isLandscape, setIsLandscape] = useState(false);
 
   const handleScroll = () => {
     if (window.scrollY > 30) {
@@ -15,21 +16,48 @@ const Header = () => {
     }
   };
 
+  const handleOrientationChange = () => {
+    if (window.innerHeight < window.innerWidth) {
+      setIsLandscape(true);
+    } else {
+      setIsLandscape(false);
+    }
+  };
+
   useEffect(() => {
-    window.scrollTo(0,0);
-    window.addEventListener('scroll', handleScroll);
+    window.scrollTo(0, 0);
+    window.addEventListener("scroll", handleScroll);
     return () => {
-      window.removeEventListener('scroll', handleScroll);
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  useEffect(() => {
+    handleOrientationChange(); // Initial check
+    window.addEventListener("resize", handleOrientationChange);
+
+    return () => {
+      window.removeEventListener("resize", handleOrientationChange);
     };
   }, []);
 
   return (
-    <div className={`header-container ${isVisible ? 'visible' : 'hidden'}`}>
-       <a href="https://wa.me/+918951217111" className="whatsapp-link" target="_blank" rel="noopener noreferrer">
-        <FontAwesomeIcon icon={faHeadset} className="whatsapp-icon" />
-        <span className="company-number">+918951217111</span>
-      </a>
-    </div>
+    <>
+      <div className={`header-container ${isVisible ? "visible" : "hidden"}`}>
+        <a
+          href="https://wa.me/+918951217111"
+          className="whatsapp-link"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FontAwesomeIcon icon={faHeadset} className="whatsapp-icon" />
+          <span className="company-number">+918951217111</span>
+        </a>
+      </div>
+      <div className={`landscape-message ${isLandscape ? "" : "hidden"}`}>
+        Please rotate your device to portrait mode.
+      </div>
+    </>
   );
 };
 
